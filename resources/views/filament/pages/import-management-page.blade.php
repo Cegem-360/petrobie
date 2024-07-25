f9f6664de96c687f9f92c18f4d7fa539fd16a0a5
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2024-07-25 11:45:53 +0200

    chore: Update ImportManagementPage to include products count

--- resources/views/filament/pages/import-management-page.blade.php
+++ resources/views/filament/pages/import-management-page.blade.php
@@ -1,6 +1,6 @@
 <x-filament-panels::page>
     <div class="">
-        @if (!$products)
+        @empty (!$products)
             <div class="">
                 @if ($productsCount > 0)
                     <x-filament::button wire:click="startGeneration" class="">Start Generation</x-filament::button>
@@ -8,16 +8,10 @@
                     <div class="p-4">
                         <p class="text-red-500">No products found</p>
                         <p class="text-red-500">Please create a products to generate</p>
+                        <x-filament::button wire:click="startDatabaseUpFill" class="">Start Flooding</x-filament::button>
                     </div>
                 @endif
             </div>
-        @else
-            <div class="">
-                @if ($productsCount <= 0)
-                    <x-filament::button wire:click="startDatabaseUpFill" class="">Start Flooding</x-filament::button>
-                @endif
-            </div>
-
-        @endif
+        @endempty
     </div>
 </x-filament-panels::page>
