b9e9e811a34737f03a368e341bba172eb6d7d15c
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2024-08-08 10:15:27 +0200

    Update `DatabaseProductDownloadFromVelodreamJob` to handle CSV content and update product information

--- app/Jobs/DatabaseProductDownloadFromVelodreamJob.php
+++ app/Jobs/DatabaseProductDownloadFromVelodreamJob.php
@@ -49,6 +49,24 @@ class DatabaseProductDownloadFromVelodreamJob implements ShouldQueue
     16 => "Akcios"
     17 => Kifuto
     ]
+    0 => "BR-X90"
+    1 => "250 03 650 52"
+    2 => "4055149337308"
+    3 => "1,27"
+    4 => "8714"
+    5 => "2950,0000"
+    6 => "3746,500000"
+    7 => "4100,0000"
+    8 => "0"
+    9 => "http://b2b.bike-parts.de/b2bdocs/BP/txt/c450a4221fcc7dab7dfa51958c162202.HTM"
+    10 => "http://b2b.bike-parts.de/b2bdocs/BP/images/8e7e93f826237de4123139492073ca78.JPG"
+    11 => "XLC Bremszugkit BR-X90"
+    12 => "1000/1350mm, Ø 1,5mm, inkl 2 Nippel"
+    13 => ""
+    14 => ""
+    15 => "False"
+    16 => "False"
+    17 => "Bowden fék MTB BR-X18"
         */
         //open csv file from velodream folder at  storage/app/velodream/velodream.csv and save to database
         $content = file_get_contents(storage_path('app/velodream/velodream.csv'));
@@ -62,19 +80,19 @@ class DatabaseProductDownloadFromVelodreamJob implements ShouldQueue
                 continue;
             }
             $prod = Product::firstOrCreate([
-                'product_id' => $product[1],
+                'product_id' => $product[0],
             ]);
 
             $prod->update(
                 [
-                    'product_id' => $product[1],
-                    'price' => $product[8],
-                    'stock' => $product[9],
-                    'product_name' => $product[0],
-                    'urlpicture' => $product[11],
-                    'barcode' =>    $product[3],
+                    'product_id' => $product[0],
+                    'price' => floatval(str_replace(',', '.', $product[7])),
+                    'stock' => $product[8],
+                    'product_name' => $product[17],
+                    'urlpicture' => $product[10],
+                    'barcode' =>    $product[2],
                     'description' => $product[15],
-                    'sku' => $product[2],
+                    'sku' => $product[1],
                 ]
             );
         }
