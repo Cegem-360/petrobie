0f3195d4cf5bf30163e6d75b6c8a061837bfddcf
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2024-08-08 11:07:54 +0200

    feat: Update GenerateProductCsvJob to include all product fields in CSV

--- app/Jobs/GenerateProductCsvJob.php
+++ app/Jobs/GenerateProductCsvJob.php
@@ -37,7 +37,24 @@ class GenerateProductCsvJob implements ShouldQueue
         $csvHeader = ['product_id', 'price', 'stock', 'product_name', 'urlpicture', 'barcode', 'description', 'sku','factory','color','size','ability','tag','source','category'];
         fputcsv($csv, $csvHeader);
         foreach ($this->products as $product) {
-            fputcsv($csv, $product->toArray());
+            $csv_product = [
+                'product_id' => $product->product_id,
+                'price' => $product->price,
+                'stock' => $product->stock,
+                'product_name' => $product->product_name,
+                'urlpicture' => $product->urlpicture,
+                'barcode' => $product->barcode,
+                'description' => $product->description,
+                'sku' => $product->sku,
+                'factory' => $product->factory,
+                'color' => $product->color,
+                'size' => $product->size,
+                'ability' => $product->ability,
+                'tag' => $product->tag,
+                'source' => $product->source,
+                'category' => $product->category,
+            ];
+            fputcsv($csv, $csv_product);
         }
     }
 }
