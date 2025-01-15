c50768092213ab33f6ac2e35ebf9ac2c8f8e3aa9
Zoltán Tamás Szabó <zoli.szabok@gmail.com>
2025-01-15 11:33:43 +0100

    feat: Enhance ExportBikefunProductsForNemetKerekpar job with batchable and dispatchable traits

--- app/Jobs/ExportBikefunProductsForNemetKerekpar.php
+++ app/Jobs/ExportBikefunProductsForNemetKerekpar.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Jobs;
 
+use Illuminate\Bus\Batchable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Storage;
 
 final class ExportBikefunProductsForNemetKerekpar implements ShouldQueue
 {
+    use Batchable;
+    use Dispatchable;
+    use InteractsWithQueue;
     use Queueable;
+    use SerializesModels;
 
     /**
      * Create a new job instance.
